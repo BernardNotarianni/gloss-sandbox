ccd508e9bbe54aef8c61c41a8a42ab192f0ce0bc
Bernard Notarianni <bernard.notarianni@gmail.com>
2018-02-17 18:05:58 +0100

    dessign un cercle noir sur fond blanc

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
 module Main where
 
-import Lib
+import Graphics.Gloss
+
+window :: Display
+window = InWindow "Gloss" (400, 400) (10, 10)
+
+background :: Color
+background = white
+
+drawing :: Picture
+drawing = circle 180
 
 main :: IO ()
-main = someFunc
+main = display window background drawing
