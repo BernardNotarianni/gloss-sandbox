dcd4ef8b279e5e1c5f2bb2f3b6156b53ebbdd325
Bernard Notarianni <bernard.notarianni@gmail.com>
2018-02-18 12:27:11 +0100

    affiche deux formes en meme temps

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # gloss-sandbox
+
 Un bacasable pour jouer avec gloss
+
+http://andrew.gibiansky.com/blog/haskell/haskell-gloss/
--- app/Main.hs
+++ app/Main.hs
@@ -9,7 +9,13 @@ background :: Color
 background = white
 
 drawing :: Picture
-drawing = circle 180
+drawing = pictures
+  [ translate (-20) (-100) $ color ballColor   $ circleSolid 30
+  , translate ( 30) (  50) $ color paddleColor $ rectangleSolid 10 50
+  ]
+  where
+    ballColor = dark red
+    paddleColor = light (light blue)
 
 main :: IO ()
 main = display window background drawing
