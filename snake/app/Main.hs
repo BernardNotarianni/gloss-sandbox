7f9681e1db6bc6b057b1e6d92c6814052a0056b3
Bernard Notarianni <bernard.notarianni@gmail.com>
2018-03-01 22:21:23 +0100

    refactor coordinate system to grid of cells

--- snake/app/Main.hs
+++ snake/app/Main.hs
@@ -13,13 +13,13 @@ fps = 3
 squareSize :: Int
 squareSize = 20
 
-data State = State { position :: (Float, Float)
-                   , velocity :: (Float, Float)
+data State = State { position :: (Int, Int)
+                   , direction :: (Int, Int)
                    }
 
 initialState :: State
-initialState = State { position = (-10, 30)
-                     , velocity = (speed, speed)
+initialState = State { position = (0, 0)
+                     , direction = (1, 0)
                      }
 
 window :: Display
@@ -33,25 +33,25 @@ update ::  Float -> State -> State
 update _seconds game = game { position = (x', y') }
   where
     (x, y) = position game
-    (vx, vy) = velocity game
+    (dx, dy) = direction game
 
-    x' = x + vx * fromIntegral squareSize
-    y' = y + vy * fromIntegral squareSize
+    x' = x + dx
+    y' = y + dy
 
 render :: State -> Picture
 render state =
   translate x y $ color headColor $ rectangleSolid 20 20
   where
-    (x,y) = position state
+    (cell_x, cell_y) = position state
+    x = fromIntegral $ cell_x * squareSize
+    y = fromIntegral $ cell_y * squareSize
     headColor = dark red
 
 handleKeys :: Event -> State -> State
 handleKeys (EventKey (Char 's') _ _ _) game = game { position = (0, 0) }
-handleKeys (EventKey (SpecialKey KeyLeft)  _ _ _) game = game { velocity = (-speed, 0) }
-handleKeys (EventKey (SpecialKey KeyRight) _ _ _) game = game { velocity = ( speed, 0) }
-handleKeys (EventKey (SpecialKey KeyUp)    _ _ _) game = game { velocity = (0, speed) }
-handleKeys (EventKey (SpecialKey KeyDown)  _ _ _) game = game { velocity = (0,-speed) }
+handleKeys (EventKey (SpecialKey KeyLeft)  _ _ _) game = game { direction = (-1, 0) }
+handleKeys (EventKey (SpecialKey KeyRight) _ _ _) game = game { direction = ( 1, 0) }
+handleKeys (EventKey (SpecialKey KeyUp)    _ _ _) game = game { direction = ( 0, 1) }
+handleKeys (EventKey (SpecialKey KeyDown)  _ _ _) game = game { direction = ( 0,-1) }
 handleKeys _ game = game
 
-speed :: Float
-speed = 1
