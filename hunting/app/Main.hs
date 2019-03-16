dc1bb00c4cf74ed32e85ffa66ee5f1e227d0aa91
Bernard Notarianni <bernard.notarianni@gmail.com>
2019-03-16 12:55:15 -0400

    animate a red box

--- hunting/app/Main.hs
+++ hunting/app/Main.hs
@@ -3,5 +3,28 @@ module Main where
 import Graphics.Gloss
 
 main :: IO ()
-main = display (InWindow "My Window" (200, 200) (10, 10)) white (Circle 80)
+-- main = display (InWindow "My Window" (200, 200) (10, 10)) white drawing
 
+
+-- step 1
+-- drawing = Circle 80
+
+-- step 2
+-- drawing = color c  $ rectangleSolid 50 50
+--   where c = dark $ dark $ dark red
+
+-- step 3
+-- drawing = Pictures   [blueBox, redBox]
+
+-- blueBox = color c  $ rectangleSolid 50 50
+--   where c = blue
+
+-- redBox = color c  $ rectangleSolid 30 30
+--   where c = red
+
+-- the target
+main = animate (InWindow "My Window" (200, 200) (10, 10)) white drawing
+
+drawing x = translate (x*10) 10 redBox
+  where
+    redBox = color red $ rectangleSolid 30 30
