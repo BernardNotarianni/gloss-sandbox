cdb8b9d9abf4f8c62d49a68b4a9048ad205d2f5c
Bernard Notarianni <bernard.notarianni@gmail.com>
2019-03-17 12:45:17 -0400

    Introduce color

--- hunting/app/Main.hs
+++ hunting/app/Main.hs
@@ -2,12 +2,20 @@ module Main where
 
 import Graphics.Gloss
 
--- we create our first function
--- it accept a parameter and avoid some form of repetition
+-- where we learn that:
+-- 1) color is beautiful and we can create great pictures
+-- 2) we can change the forms
+-- 3) we do not use parenthesis. we name everything and it is easier
+--    to understand what's going on. It is especially usefull and efficient
+--    in langage hunting mode (ie no speaking allowed)
 
 main = display (InWindow "My window" (300, 500) (200, 200))
        white drawing
 
-drawing = square 50
+drawing = Color red mediumSquare
+
+mediumSquare = square 50
 
 square x = rectangleSolid x x
+
+
