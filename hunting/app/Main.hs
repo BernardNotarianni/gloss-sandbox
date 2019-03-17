e7ee3459f6fa9e546e368d5282abd02a32c37704
Bernard Notarianni <bernard.notarianni@gmail.com>
2019-03-17 12:05:42 -0400

    play with parameters

--- hunting/app/Main.hs
+++ hunting/app/Main.hs
@@ -2,5 +2,9 @@ module Main where
 
 import Graphics.Gloss
 
-main = display (InWindow "My window" (500, 500) (200, 200))
-       white (Circle 80)
+-- play with some parameters
+-- we learn it is ok to play with code within short feedback loops
+-- to discover how it works.
+
+main = display (InWindow "My window" (300, 500) (200, 200))
+       white (Circle 120)
